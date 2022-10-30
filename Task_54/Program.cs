1e6d9e068442261c6bc8cb3708ea61fef32a6f02
Arcadiy <Motor-person@yandex.ru>
2022-10-30 14:29:41 +0400

    Сделали Task_54

---
+++ Task_54/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.Clear(); // метод замены столбцов и строк местами
+
+Console.WriteLine("Пожалуйста, введите количество строк матрицы");
+int rowsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите количество столбцов матрицы");
+int columsMatrix = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите минимально возможный элемент матрицы");
+int minimal = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите максимально возможный элемент матрицы");
+int maximal = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+
+int[,] matrix = CreateMatrix(rowsMatrix, columsMatrix, minimal, maximal);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+
+int[,] CreateMatrix(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        System.Console.Write(("["));
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1) - 1) System.Console.Write($"{matr[i, j],4}, ");
+            else System.Console.Write($"{matr[i, j],4}");
+        }
+        System.Console.WriteLine(("]"));
+    }
+}
+
+
+void RangeMatrixLines(int[,] matr)
+{
+  for (int i = 0; i < matr.GetLength(0); i++)
+  {
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+      for (int k = 0; k < matr.GetLength(1) - 1; k++)
+      {
+        if (matr[i, k] < matr[i, k + 1])
+        {
+          int temp = matr[i, k + 1];
+          matr[i, k + 1] = matr[i, k];
+          matr[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+RangeMatrixLines(matrix);
+PrintMatrix(matrix);
